40b06777e57b41fc99802ba967c8e01b867fcae1
Levy Cen <levycen.d@gmail.com>
2021-10-13 22:00:13 -0500

    Manipular el patron MVC

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def hello
+    @variable = "Hola soy #{Pet.first.name} y soy un #{Pet.first.breed}"
   end
 end
---
+++ comandosEjemplo.rb
@@ -0,0 +1,3 @@
+# Video 9 - Manipular el patrón MVC - Platzi
+# Crear un registro de Pet
+Pet.create name:'Lulu', breed: 'poodle'
