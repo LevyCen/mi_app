35a10bc63131142edfd7e8cb39c2d015096dec23
Levy Cen <levycen.d@gmail.com>
2021-10-25 16:35:40 -0500

    Scaffolds and migrations

--- comandosEjemplo.rb
+++ comandosEjemplo.rb
@@ -16,3 +16,23 @@ Para producción:
 RAILS_ENV=production rails c
 Rails.application.credentials.config # nos mostrara todo el contenido
 Rails.application.credentials.aws # nos mostrara el valor de la clave aws
+
+
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/github_levycen
+
+Video 12 - Agregar el primer conjunto de scaffolds - Platzi
+
+# scafold genera toda la estrucutra necesaria para construit modelos vistas y controladores necesarios para gestionar el concepto de book
+rails g scaffold Book title:string author:string description:text isbn:string page_count:integer
+
+
+13 - Cómo funcionan las migraciones - Platzi
+rails s
+http://localhost:3000
+rails db:migrate #del mundo de ruby => al lenguaje de DB
+
+http://localhost:3000/books
+
+#ver el status de la smigraciones
+rails db:migrate:status
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_024233) do
+ActiveRecord::Schema.define(version: 2021_10_25_191629) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "page_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
